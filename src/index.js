677151e79d4db8a7fe14f0ed9797b2fa2d776e0e
Momoy369 <ronijagat@gmail.com>
2025-03-28 02:47:37 +0800

    index

--- src/index.js
+++ src/index.js
@@ -1,13 +1,13 @@
 import React from "react";
-import ReactDOM from "react-dom";
+import ReactDOM from "react-dom/client"; // Pastikan menggunakan "react-dom/client"
 import App from "./App";
 import { WalletProviderComponent } from "./WalletProvider";
 
-ReactDOM.render(
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
   <React.StrictMode>
     <WalletProviderComponent>
       <App />
     </WalletProviderComponent>
-  </React.StrictMode>,
-  document.getElementById("root")
+  </React.StrictMode>
 );
