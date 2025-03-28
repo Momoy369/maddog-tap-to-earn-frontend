386fbdc74bd23d847fba73b0cb6907d8bb11e6ee
Momoy369 <ronijagat@gmail.com>
2025-03-29 07:25:55 +0800

    khdas

---
+++ src/pages/Profile.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function Profile() {
+  return (
+    <div className="text-center">
+      <h1 className="text-2xl font-bold">👤 Profil</h1>
+      <p>Informasi akun pengguna.</p>
+    </div>
+  );
+}
