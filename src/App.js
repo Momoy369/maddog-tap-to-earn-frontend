072e6a471918c6108834c9ba8682dd8b9d044bea
Momoy369 <ronijagat@gmail.com>
2025-03-29 08:12:47 +0800

    tes

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ import Profile from "./pages/Profile";
 export default function App() {
   return (
     <Router>
-      <div className="container mx-auto flex flex-col items-center justify-center min-h-screen bg-gray-900 text-white px-6 py-10">
+      <div className="min-h-screen bg-gray-900 text-white flex flex-col items-center justify-center">
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/about" element={<About />} />
